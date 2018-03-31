ee67ccd56cc70a59f03e0fdba50a3aae8c7fadc5
Igor Eydman <igor@Igors-MBP.home>
2018-03-31 11:26:22 -0400

    Added CoreML with SqueezeNet.mlmodel

--- ItemHunt/Controller/ItemHuntVC.swift
+++ ItemHunt/Controller/ItemHuntVC.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ItemHuntVC: UIViewController {
     
     @IBOutlet weak var cameraImage: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
     
     let imagePicker = UIImagePickerController()
     
@@ -18,6 +21,7 @@ class ItemHuntVC: UIViewController {
         super.viewDidLoad()
         
         imagePicker.delegate = self
+        
     }
     
     @IBAction func CameraTapped(_ sender: Any) {
@@ -26,22 +30,63 @@ class ItemHuntVC: UIViewController {
         imagePicker.cameraCaptureMode = .photo
         self.present(imagePicker, animated: true, completion: nil)
     }
+    
+    func classifyImage(image: CIImage) {
+        
+        classificationLabel.text = "detecting object..."
+        
+        // Load the ML model through generated class
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {
+            fatalError("can't load ML model")
+        }
+        
+        // Vision request
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            // Update UI
+            DispatchQueue.main.async { [weak self] in
+                self?.classificationLabel.text = "\(Int(topResult.confidence * 100))% \(topResult.identifier) detected"
+            }
+        }
+        
+        // Running classifier on global dispatch que
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
 }
 
 extension ItemHuntVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        SetImage(image: chosenImage)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("couldn't load image")
+        }
+        
+        cameraImage.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        // Send to CoreML
+        classifyImage(image: ciImage)
         
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
-    }
-    
-    func SetImage(image: UIImage) {
-        cameraImage.image = image
     }
 }
