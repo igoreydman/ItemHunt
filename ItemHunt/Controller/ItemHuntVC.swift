c7248870c4cc67e883d15c6bb3a861826dbeb656
Igor Eydman <igor@Igors-MBP.home>
2018-03-31 07:37:18 -0400

    Added UIImagePicker

--- ItemHunt/Controller/ItemHuntVC.swift
+++ ItemHunt/Controller/ItemHuntVC.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class ItemHuntVC: UIViewController {
-
+    
+    @IBOutlet weak var cameraImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        imagePicker.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func CameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.cameraCaptureMode = .photo
+        self.present(imagePicker, animated: true, completion: nil)
     }
-    */
+}
 
+extension ItemHuntVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        SetImage(image: chosenImage)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func SetImage(image: UIImage) {
+        cameraImage.image = image
+    }
 }
